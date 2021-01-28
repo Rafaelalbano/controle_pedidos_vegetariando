45307aaa4f1da76a69cbec11db92b7e3f75c8110
Rafael Albano <rafaelb.albano@gmail.com>
2021-01-28 13:39:28 -0300

    Feito os campos de login, integração PDO e verificação de usuário.

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+
+    if(count($_POST)>0){
+
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=vegetariando_db", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Conexão realizada com sucesso!";
+    $stmt = $conn->prepare("SELECT codigo FROM usuario WHERE email=:email AND senha=md5(:senha)");
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
+    $stmt->execute();
+    
+    // set the resulting array to associative
+        $result = $stmt->fetchAll();
+        $qtd_usuarios = count($result);
+        if($qtd_usuarios == 1){
+            $resultado["msg"] = "Usuário encontrado!";
+            $resultado["cod"] = 1;
+        }else if($qtd_usuarios == 0){
+            $resultado["msg"] =  "E-mail e senha não conferem.";
+            $resultado["cod"] = 0;
+        }
+    }
+    catch(PDOException $e) {
+        echo "Conexão falhou: " . $e->getMessage();
+        }
+    }
+        $conn = null;
+       
+        include("index.php");
+    
+    
+
+?>
